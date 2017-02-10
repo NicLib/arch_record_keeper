cfc730ce3c0aeaf90611c79f89108dfbf83eb325
Preston Howell <pshowell23@gmail.com>
2017-02-10 14:08:32 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :users
   resources :jobs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root 'jobs#index'
-  
-  get "job/totals", to: "jobs#totals", as: "totals"
+
+  get "/totals", to: "jobs#totals"
 end
