7ad2050319dba265b9bf5aab6e95bd76364e65fb
Preston Howell <pshowell23@gmail.com>
2017-02-16 11:49:27 -0500

    Add method for test login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
     validates :username, presence: true
     has_secure_password
+
+    #Returns the has digest of the given string
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engin.cost
+
+        Bcrypt::Password.create(string, cost: cost)
+    end
 end
