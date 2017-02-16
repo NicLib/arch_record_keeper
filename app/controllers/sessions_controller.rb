b0b5b931b4b78e1c73370e325bf10f8e17091754
Preston Howell <pshowell23@gmail.com>
2017-02-16 11:27:55 -0500

    Add login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+      user = User.find_by(username: params[:session][:username].downcase)
+
+      if user && user.authenticate(params[:session][:password])
+          log_in user
+          redirect_to jobs_path
+      else
+          flash.now[:danger] = 'Invalid username or password'
+          render 'new'
+      end
+  end
+
+  def destroy
+  end
 end
