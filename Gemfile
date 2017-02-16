55c09df6e785b2720b07cc4d1028f68b19c17d1a
Preston Howell <pshowell23@gmail.com>
2017-02-16 11:08:31 -0500

    Add Bootstrap

--- Gemfile
+++ Gemfile
@@ -50,3 +50,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'chartkick'
 gem 'bcrypt', '3.1.11'
+gem 'bootstrap-sass', '3.3.6'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
-//= require_tree .
 //= require Chart.bundle
 //= require chartkick
+//= require_tree .
