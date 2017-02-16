0bce365febf08b6a4b90919db83138c9ab871d7e
Preston Howell <pshowell23@gmail.com>
2017-02-16 12:36:16 -0500

    Add styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,20 @@
 module ApplicationHelper
-  
+
+  #Makes table columns sortable
   def sortable(column, title = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
     direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+  end
+
+  #Provides full title for each page of app
+  def full_title(page_title = '')
+      base_title = 'Nicholson Library Archives of Anderson University'
+      if page_title.empty?
+          base_title
+      else
+          page_title + ' | ' + base_title
+      end
   end
 end
