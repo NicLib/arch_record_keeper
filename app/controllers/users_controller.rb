940288f0fed632be5bf153a0c31ae7204bf2313b
Preston Howell <pshowell23@gmail.com>
2017-02-17 12:45:44 -0500

    Allow users to be saved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -78,6 +78,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :username)
+      params.require(:user).permit(:first_name, :last_name, :username, :password, :password_confirmation)
     end
 end
