6ac700c698e9f005c63d76d4fdbfb158c60b557d
Preston Howell <pshowell23@gmail.com>
2017-02-17 11:11:13 -0500

    Add some formatting

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,4 +2,9 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
 
+    def setup
+        @user = User.new(username: 'example', password: 'foobar', password_confirmation: 'foobar')
+    end
+
+    
 end
